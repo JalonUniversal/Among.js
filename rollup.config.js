a43e24d632886b7837d4d06656e1a6363ee87650
JalonUniversal <453741411@qq.com>
2019-11-30 19:19:32 +0800

    docs: change build config & project config

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,23 @@
 const resolve = require('rollup-plugin-node-resolve');
 const babel = require('rollup-plugin-babel');
+const commonjs = require('rollup-plugin-commonjs');
 
 export default {
   input: 'lib/main.js',
   output: {
     file: 'build/bundle.js',
     format: 'umd',
-    name: 'Among'
+    name: 'among',
+    globals: {
+      among: 'among'
+    }
   },
   plugins: [
     resolve(),
+    commonjs(),
     babel({
       exclude: 'node_modules/**',
-      
+
     })
   ]
 }
